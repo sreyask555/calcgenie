05e6611bdd9f5ac16de92587f9efda07161ec1fc
Sreyas <sreyask555@gmail.com>
2022-07-23 01:00:52 +0530

    raw-js initial

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // CalcEngine
+
+//Default Values
+const default_value = ""
+document.getElementById('calc-disp').innerHTML = '5'
+
+// Math Operands
+
+
+// Math Calculation
+showMath = (id_value) => {
+    document.getElementById('calc-disp').innerHTML += document.getElementById(id_value)
+}
+
+doMath = () => {
+    let math_expression = document.getElementById('calc-disp').value;
+    let calc_value = math_expression
+    console.log(calc_value)
+}
